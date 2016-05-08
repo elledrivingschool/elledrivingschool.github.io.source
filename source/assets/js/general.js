e5295905e43776287cb76fa9b976ace4d5457976
Mike Parkin <mparkin@de-facto.com>
2016-05-08 20:24:56 +0100

    Remove unrequired JS files

--- source/assets/js/general.js
+++ source/assets/js/general.js
@@ -1,6 +1,6 @@
 (function (window) {
     'use strict';
-    
+
     $(document).on('click', '#menu', function (e) {
         $('.hidden-menu ').toggleClass('open');
         $(this).toggleClass('close');
